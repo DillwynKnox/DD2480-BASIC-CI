19d99302893cea2672c76279d0fd97f9e08b29e2
DillwynKnox <anton.trappe@gmx.de>
2026-02-06 11:34:15 +0100

    print payload

--- src/basic_ci/api/webhook.py
+++ src/basic_ci/api/webhook.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 
+from basic_ci.schemes.push_payload import PushPayload
+
 router = APIRouter(tags=["webhook"])
 
 @router.post("/webhook")
 def handle_webhook(payload: dict) -> dict[str, str]:
     """Handle incoming webhook payloads."""
     print("Received webhook payload:", payload)
+    push_payload = PushPayload(*payload)
+    print("================")
+    print(push_payload)
     return {"status": "received"}
