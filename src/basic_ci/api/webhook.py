25185562a6ee46dcfac174c835c38ca691912ece
Anton <Anton@privat.tk>
2026-02-07 00:19:29 +0100

    Webhook is there

--- src/basic_ci/api/webhook.py
+++ src/basic_ci/api/webhook.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
+from fastapi.params import Header
 
 from basic_ci.schemes.push_payload import Push_payload
 
 router = APIRouter(tags=["webhook"])
 
 @router.post("/webhook")
-def handle_webhook(payload: dict) -> dict[str, str]:
+def handle_webhook(
+            payload: dict,
+            x_hub_signature_256: str | None = Header(None)
+        ) -> dict[str, str]:
     """Handle incoming github webhook payloads."""
-    print("Received webhook payload:", payload)
     push_payload = Push_payload.model_validate(payload)
-    print("================")
     print(push_payload)
     return {"status": "received"}
