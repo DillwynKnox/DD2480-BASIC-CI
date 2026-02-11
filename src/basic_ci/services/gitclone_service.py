02cc3cc564fe06fe373f8b7129fc642a28928ffe
Daxmeister <113617175+Daxmeister@users.noreply.github.com>
2026-02-11 13:53:30 +0100

    [FEAT] Create Service that allows interaction with GIT

---
+++ src/basic_ci/services/gitclone_service.py
@@ -0,0 +1,17 @@
+from git import Repo #  git = gitPython package
+
+
+class GitcloneService:
+    """
+    Clones a repo into a directory and checks out given commit.
+    """
+    def __init__(self, repo_url: str):
+        self.repo_url = repo_url
+    
+    def clone_repo(self, head_commit_hash: str, directory: str):
+        
+        repo = Repo.clone_from(self.repo_url, directory)
+        repo.git.checkout(head_commit_hash)
+
+    
+    
