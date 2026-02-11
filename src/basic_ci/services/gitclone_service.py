e25a95fe0bbd227ea416c83bfe5526755f7261a7
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2026-02-11 13:35:36 +0100

    changed what init functino takes

--- src/basic_ci/services/gitclone_service.py
+++ src/basic_ci/services/gitclone_service.py
@@ -5,14 +5,13 @@ class GitcloneService:
     """
     Clones a repo into a directory and checks out given commit.
     """
-    def __init__(self, repo_url: str, head_commit_hash: str, directory: str):
+    def __init__(self, repo_url: str):
         self.repo_url = repo_url
-        self.commit_hash = head_commit_hash
-        self.directory = directory
     
-    def clone_repo(self):
-        repo = Repo.clone_from(self.repo_url, self.directory)
-        repo.git.checkout(self.commit_hash)
+    def clone_repo(self, head_commit_hash: str, directory: str):
+        
+        repo = Repo.clone_from(self.repo_url, directory)
+        repo.git.checkout(head_commit_hash)
 
     
     
