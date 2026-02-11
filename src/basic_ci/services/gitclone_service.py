bf8117e69f013de01a5f203a2ac946c5f8b58705
jribi <mohamed.jribi@epfl.ch>
2026-02-11 17:26:28 +0100

    [FIX] fix errors of typecheck

--- src/basic_ci/services/gitclone_service.py
+++ src/basic_ci/services/gitclone_service.py
@@ -5,10 +5,10 @@ class GitcloneService:
     """
     Clones a repo into a directory and checks out given commit.
     """
-    def __init__(self, repo_url: str):
+    def __init__(self, repo_url: str) -> None:
         self.repo_url = repo_url
     
-    def clone_repo(self, head_commit_hash: str, directory: str):
+    def clone_repo(self, head_commit_hash: str, directory: str) -> None:
         
         repo = Repo.clone_from(self.repo_url, directory)
         repo.git.checkout(head_commit_hash)
